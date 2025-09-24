9ff7c33daa9bb10f167b115085c8ae7b5c01d106
Willie <williearaeb3@gmail.com>
2025-09-24 02:00:33 +0200

    Add files via upload
    Database Configurations

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 class DatabaseConfig {
     const HOST = 'localhost';
     const USERNAME = 'root';
-    const PASSWORD = 'root';
+    const PASSWORD = '';
     const DATABASE = 'mesmtf_db';
     const CHARSET = 'utf8mb4';
 }
