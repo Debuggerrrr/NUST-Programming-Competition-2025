54fcd3c011dbd6db8e94772f5c6af0910b6eabcb
Willie <williearaeb3@gmail.com>
2025-09-27 09:14:55 +0200

    Delete config directory

--- config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-class DatabaseConfig {
-    const HOST = 'localhost';
-    const USERNAME = 'root';
-    const PASSWORD = '';
-    const DATABASE = 'mesmtf_db';
-    const CHARSET = 'utf8mb4';
-}
-?>
